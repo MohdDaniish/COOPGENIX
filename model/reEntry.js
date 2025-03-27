0ad9ba7c8f1df220a8fb5c3aaa8d1beb7babe8ac
MohdDaniish <danish.ekaasha@gmail.com>
2025-03-27 12:37:13 +0530

    first commit

---
+++ model/reEntry.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reEntrySchema = new Schema({
+    user: {
+        type: String,
+        required: true
+    },
+    martixId: {
+        type: Number,
+        default:0
+    },
+    slotId: {
+        type: Number,
+        required: true
+    },
+    level: {
+        type: Number,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    txHash: { type: String, required: true, },
+    block: { type: Number, required: true },
+    timestamp: { type: Number, required: true },
+});
+
+reEntrySchema.index(
+    { user: 1, martixId: 1, slotId : 1, level :1, txHash: 1 },
+    { unique: true }
+  );
+
+const reEntry = mongoose.model('reentri', reEntrySchema);
+
+module.exports = reEntry;
